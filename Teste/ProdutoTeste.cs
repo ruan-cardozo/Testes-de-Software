49ed2cae16538dbfde37348b70450ef366663e12
ruan-cardozo <ruanvcardozo@gmail.com>
2023-09-13 20:05:05 -0300

    add comentarios

--- Teste/ProdutoTeste.cs
+++ Teste/ProdutoTeste.cs
@@ -6,12 +6,23 @@ using Xunit;
 
 namespace Teste
 {
+    /* 
+    Eu, como almoxarife, preciso cadastrar produtos para que sejam consumidos pleos funcionários da empresa
+
+    Critérios de aceite?
+    1 - Um novo produto deve ter obrigatoriamente um código único, nome,
+    saldo 0, custo 0 e unidade de medida
+    2 - As unidades de medida são PC, KG, MT, GR, LT, CX
+    3 - um produto PODE ter uma descrição
+    4 - Um produto PODE ter um código de barras EAN
+    */
+
     public class ProdutoTeste
     {
         [Fact]
         public void Test1()
         {
-            Assert.True(true);
+            
         }
     }
 }
